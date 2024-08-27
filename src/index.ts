c61f3664403163f3bc05178877078f67d3f48262
albanog00 <albanog00@gmail.com>
2024-08-27 14:36:22 +0200

    update test case

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { BinarySerializer } from "./serializer/serializer";
 import { Person } from "./person";
 
-const person = new Person("Peppe", 23, [
-  0,
-  1,
-  (1 << 8) - 1,
-  -1,
-  (1 << 16) - 1,
-  Math.pow(2, 31) - 1,
-  -Math.pow(2, 16),
-  -Math.pow(2, 31) + 1,
-]);
+const person = new Person(
+  "Peppe",
+  23,
+  new Array<number>(65534).fill(Math.pow(2, 31) - 1),
+);
 
 const buffer = BinarySerializer.encode(person);
 console.log(buffer);

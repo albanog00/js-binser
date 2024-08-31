59181c4c75575743258bb7b04043ca7bd476d8d4
albanog00 <albanog00@gmail.com>
2024-08-31 12:58:00 +0200

    fix test

--- tests/writer.test.ts
+++ tests/writer.test.ts
@@ -1,11 +1,15 @@
-import { describe, it } from "node:test";
 import assert = require("node:assert");
+import { describe, it } from "node:test";
 import { BinaryWriter } from "../src/serializer/writer";
 
 describe("writer tests", () => {
   it("should write uint8 like Buffer", () => {
     const writer = new BinaryWriter();
+    const buffer = Buffer.alloc(1);
+
     writer.writeUInt8(128);
-    assert.ok(Buffer.from([128]).compare(writer.data));
+    buffer.writeUInt8(128);
+
+    assert.equal(buffer.compare(writer.data), 0);
   });
 });

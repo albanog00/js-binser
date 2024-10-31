d8000e78b9f26f8048c5c68324dc3063259de38b
Giuseppe <albanog00@gmail.com>
2024-10-31 13:52:38 +0100

    refactor

--- benchmark/benchmark.ts
+++ benchmark/benchmark.ts
@@ -1,98 +1,69 @@
 import { BinarySerializer } from "../src/serializer/serializer";
 import { Person } from "../src/person";
 
-console.log("Creating objects...");
+console.log("Initializing objects...");
 
-const person = new Person(
-  "Peppe",
-  23,
-  new Array<number>(1_000_000).fill(1),
-);
-
-const lengthTestArray = 10;
 const iterations = 10;
-const innerIterations = 1;
+const inner_iterations = 10;
 
-const personArray = new Array<Person>(lengthTestArray).fill(person);
-const jsonArray = new Array<string>(lengthTestArray).fill(
-  JSON.stringify(person),
-);
-const bytesArray = new Array<Buffer>(lengthTestArray).fill(
-  BinarySerializer.encode(person),
-);
+const obj = new Person("Peppe", 23, new Array<number>(1_000_000).fill(1));
+const json = JSON.stringify(obj);
+const bytes = BinarySerializer.encode(obj);
+
+function benchmark(label: string, fn: () => void): number {
+	let total_execution_time = 0;
+	for (let j = 0; j < iterations; ++j) {
+		const start_iteration = Date.now();
+		for (let i = 0; i < inner_iterations; ++i) {
+			fn();
+		}
+
+		const execution_time = Date.now() - start_iteration;
+		console.log(`Execution time for ${label}: ${execution_time}ms`);
+		total_execution_time += execution_time;
+	}
+	return total_execution_time;
+}
+
+function logTimings(label: string, total: number) {
+	console.log(
+		`${label} \ttotal time ${total}ms \tavg time: ${
+			total / iterations
+		}ms \top/s: ${(1000 / (total / (iterations * inner_iterations))).toFixed(2)} ${
+			label[label.indexOf(" ") + 1] === "s"
+				? `\tencoded size: ${
+						label[0] === "j" ? json.length : bytes.buffer.byteLength
+					} bytes`
+				: ""
+		}`,
+	);
+}
 
 console.log("Starting benchmark...");
 
-function Benchmark(label: string, fn: () => void): number {
-  let start = Date.now();
-
-  for (let j = 0; j < iterations; ++j) {
-    const startIteration = Date.now();
-
-    for (let i = 0; i < innerIterations; ++i) fn();
-
-    const executionTime = Date.now() - startIteration;
-
-    console.log(`Execution time for ${label}: ${executionTime}ms`);
-  }
-
-  return Date.now() - start;
-}
-
-const jsonSerTotal = Benchmark("json serializer", () => {
-  for (const person of personArray) JSON.stringify(person);
+console.log();
+const json_ser_total = benchmark("json serializer", () => {
+	JSON.stringify(obj);
 });
 
-console.log("\n");
-
-const binarySerTotal = Benchmark("binary serializer", () => {
-  for (const person of personArray) BinarySerializer.encode(person);
+console.log();
+const binary_ser_total = benchmark("binary serializer", () => {
+	BinarySerializer.encode(obj);
 });
 
-console.log("\n");
-
-const jsonDeTotal = Benchmark("json deserializer", () => {
-  for (const json of jsonArray) JSON.parse(json);
+console.log();
+const json_de_total = benchmark("json deserializer", () => {
+	JSON.parse(json);
 });
 
-console.log("\n");
-
-const binaryDeTotal = Benchmark("binary deserializer", () => {
-  for (const bytes of bytesArray) BinarySerializer.decode(bytes);
+console.log();
+const binary_de_total = benchmark("binary deserializer", () => {
+	BinarySerializer.decode(bytes);
 });
 
-console.log("\n");
-console.log(" --------------------------- ");
-console.log("\n");
+console.log("\n ---------------------------- \n");
 
-console.log(
-  `json serializer \ttotal time ${jsonSerTotal}ms \tavg time: ${
-    jsonSerTotal / iterations
-  }ms \top/s: ${
-    jsonSerTotal / (iterations * innerIterations * lengthTestArray)
-  }ms \tencoded size: ${jsonArray[0].length} bytes`,
-);
-
-console.log(
-  `binary serializer \ttotal time ${binarySerTotal}ms \tavg time: ${
-    binarySerTotal / iterations
-  }ms \top/s: ${
-    binarySerTotal / (iterations * innerIterations * lengthTestArray)
-  }ms \t encoded size: ${bytesArray[0].length} bytes`,
-);
-
-console.log(
-  `json deserializer \ttotal time ${jsonDeTotal}ms \tavg time: ${
-    jsonDeTotal / iterations
-  }ms \top/s: ${
-    jsonDeTotal / (iterations * innerIterations * lengthTestArray)
-  }ms`,
-);
-
-console.log(
-  `binary deserializer \ttotal time ${binaryDeTotal}ms \tavg time: ${
-    binaryDeTotal / iterations
-  }ms \top/s: ${
-    binaryDeTotal / (iterations * innerIterations * lengthTestArray)
-  }ms`,
-);
+logTimings("json serializer", json_ser_total);
+logTimings("binary serializer", binary_ser_total);
+logTimings("json deserializer", json_de_total);
+logTimings("binary deserializer", binary_de_total);
